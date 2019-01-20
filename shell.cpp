1fc600a1bd5f61c3fca0b75dc0a0699598c99266
Danny Cho <dannycho7@gmail.com>
2019-01-20 10:51:29 -0800

    Enable file redirect for cmd

--- shell.cpp
+++ shell.cpp
@@ -1,14 +1,24 @@
 #include <algorithm>
+#include <fcntl.h>
 #include <iostream>
 #include <sys/wait.h>
 #include <unistd.h>
 #include <vector>
 #include "parser.h"
 
-bool readCmd(std::string& cmd) {
-	std::cout << "Shell: ";
-	std::getline(std::cin, cmd);
-	return !std::cin.eof();
+void adjustForRedirect(program_cmd_t cmd) {
+	if (cmd.does_redirect_files[0]) {
+		int rd_fd = open(cmd.redirect_files[0].c_str(), O_RDONLY);
+		int dup_res = dup2(rd_fd, STDIN_FILENO);
+		close(rd_fd);
+	}
+
+	if (cmd.does_redirect_files[1]) {
+		mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+		int wrt_fd = open(cmd.redirect_files[1].c_str(), O_WRONLY|O_CREAT|O_TRUNC, mode);
+		int dup_res = dup2(wrt_fd, STDOUT_FILENO);
+		close(wrt_fd);
+	}
 }
 
 char** const convertArgsForExec(const std::vector<std::string>& cmd_args) {
@@ -21,6 +31,12 @@ char** const convertArgsForExec(const std::vector<std::string>& cmd_args) {
 	char** args = new char*[cmd_buf.size()];
 	std::copy(cmd_buf.begin(), cmd_buf.end(), args);
 	return args;
+}
+
+bool readCmd(std::string& cmd) {
+	std::cout << "Shell: ";
+	std::getline(std::cin, cmd);
+	return !std::cin.eof();
 }
 
 int main() {
@@ -39,6 +55,8 @@ int main() {
 		pid_t pid = fork();
 		if (pid == 0) {
 			auto args = convertArgsForExec(cmd.args);
+			adjustForRedirect(cmd);
+
 			execvp(cmd.program_exec.c_str(), args);
 			free(args);
 			exit(0);
