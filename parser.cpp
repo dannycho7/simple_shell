90a4b9e44b029809552885fb3fc3af88722c70e8
Danny Cho <dannycho7@gmail.com>
2019-01-20 14:24:58 -0800

    Fix parse error on trailing whitespace + Use npos as terminating case for parser

--- parser.cpp
+++ parser.cpp
@@ -20,12 +20,11 @@ namespace Parser {
 	std::string getNextToken(std::string program_cmd, int& start_pos) {
 		int token_start_i = program_cmd.find_first_not_of(" ", start_pos);
 		if (program_cmd[token_start_i] == '<' || program_cmd[token_start_i] == '>') {
-			start_pos = program_cmd.find_first_not_of(" ", token_start_i + 1);
+			start_pos = token_start_i;
 			return program_cmd.substr(token_start_i, 1);
 		} else {
 			int token_end_i = program_cmd.find_first_of(">< ", token_start_i);
-			token_end_i = ((token_end_i == std::string::npos) ? program_cmd.length() - 1 : token_end_i - 1);
-			start_pos = program_cmd.find_first_not_of(" ", token_end_i + 1);
+			start_pos = token_end_i;
 
 			return program_cmd.substr(token_start_i, token_end_i - token_start_i + 1);
 		}
@@ -34,27 +33,25 @@ namespace Parser {
 	program_cmd_t getParsedProgramCmd(const std::string program_cmd) {
 		int program_exec_i_start = program_cmd.find_first_not_of(" ");
 		int program_exec_i_end = program_cmd.find_first_of(">< ", program_exec_i_start);
-		program_exec_i_end = ((program_exec_i_end == std::string::npos)
-			? program_cmd.length() - 1 : program_exec_i_end - 1);
-
 		program_cmd_t parsed_program_cmd;
-		std::string program_exec = program_cmd.substr(program_exec_i_start, program_exec_i_end - program_exec_i_start + 1);
+		std::string program_exec = program_cmd.substr(program_exec_i_start, program_exec_i_end - program_exec_i_start);
 		parsed_program_cmd.program_exec = program_exec;
 		parsed_program_cmd.args.push_back(program_exec);
 
-		int current_i = program_exec_i_end + 1;
-		while (current_i < program_cmd.length()) {
+		int current_i = program_cmd.find_first_not_of(" ", program_exec_i_end);
+		while (current_i != std::string::npos) {
 			std::string token = getNextToken(program_cmd, current_i);
 
 			if (token == "<") {
 				parsed_program_cmd.does_redirect_files[0] = true;
-				parsed_program_cmd.redirect_files[0] = getNextToken(program_cmd, current_i);
+				parsed_program_cmd.redirect_files[0] = getNextToken(program_cmd, ++current_i);
 			} else if (token == ">") {
 				parsed_program_cmd.does_redirect_files[1] = true;
-				parsed_program_cmd.redirect_files[1] = getNextToken(program_cmd, current_i);
+				parsed_program_cmd.redirect_files[1] = getNextToken(program_cmd, ++current_i);
 			} else {
 				parsed_program_cmd.args.push_back(token);
 			}
+			current_i = program_cmd.find_first_not_of(" ", current_i);
 		}
 
 		return parsed_program_cmd;
