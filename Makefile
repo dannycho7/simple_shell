08368ebee2a77b437a41986e133cbd4e1f0ef775
Danny Cho <dannycho7@gmail.com>
2019-01-19 23:35:07 -0800

    Impl for executing first command

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 main: shell.o parser.o
 	g++ -std=c++11 shell.o parser.o -o shell.out
-shell.o:
+shell.o: shell.cpp parser.h
 	g++ -std=c++11 -c shell.cpp
-parser.o:
+parser.o: parser.cpp parser.h
 	g++ -std=c++11 -c parser.cpp
 clean:
 	rm *.out *.o
--- shell.cpp
+++ shell.cpp
@@ -1,4 +1,7 @@
+#include <algorithm>
 #include <iostream>
+#include <sys/wait.h>
+#include <unistd.h>
 #include <vector>
 #include "parser.h"
 
@@ -6,6 +9,18 @@ bool readCmd(std::string& cmd) {
 	std::cout << "Shell: ";
 	std::getline(std::cin, cmd);
 	return !std::cin.eof();
+}
+
+char** const convertArgsForExec(const std::vector<std::string>& cmd_args) {
+	std::vector<char *> cmd_buf(cmd_args.size());
+	std::transform(cmd_args.begin(), cmd_args.end(), cmd_buf.begin(), [](const std::string& arg) {
+		return const_cast<char*>(arg.c_str());
+	});
+
+	cmd_buf.push_back(nullptr);
+	char** args = new char*[cmd_buf.size()];
+	std::copy(cmd_buf.begin(), cmd_buf.end(), args);
+	return args;
 }
 
 int main() {
@@ -17,6 +32,20 @@ int main() {
 		}
 
 		auto parsed_cmd = Parser::getParsedCmd(cmd);
-		Parser::printParsedCmd(parsed_cmd);
+		// Parser::printParsedCmd(parsed_cmd);
+
+		program_cmd_t cmd = parsed_cmd[0];
+
+		pid_t pid = fork();
+		if (pid == 0) {
+			auto args = convertArgsForExec(cmd.args);
+			execvp(cmd.program_exec.c_str(), args);
+			free(args);
+			exit(0);
+		} else {
+			int status;
+			wait(&status);
+			std::cout << " STATUS: " << status << std::endl;
+		}
 	}
 }
